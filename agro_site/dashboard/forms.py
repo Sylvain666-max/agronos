a8020e9235a699ab99911f50986658c8ecd6d1e1
da-COSTA Togninou <dacostatogninou@gmail.com>
2025-09-20 18:28:43 +0200

    Nouvelle Version

--- agro_site/dashboard/forms.py
+++ agro_site/dashboard/forms.py
@@ -2,12 +2,7 @@ from django import forms
 from agro_site.blog.models import Post
 from agro_site.shop.models import Product
 from agro_site.services.models import Service
-
-
-
-
-
-from blog.models import Post
+from agro_site.blog.models import Post
 
 class PostForm(forms.ModelForm):
     class Meta:
